13ff27dfc0eb4cfc9ed1e99ea7980cbd99890f78
namanverma18009@gmail.com <namanverma18009@gmail.com>
2021-10-06 17:56:20 +0530

    second commit

--- server/DatabaseApis/DatabaseClient.js
+++ server/DatabaseApis/DatabaseClient.js
@@ -1,6 +1,6 @@
 const {MongoClient} = require("mongodb");
 
-const url = "mongodb+srv://namanverma:namanverma@zero.za8kh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const url = "";
 
 const client = new MongoClient(url);
  client.connect();
